bafe2c2911dc73e34d31e8072b6c85e951223afc
Ilyas <jugj@users.noreply.github.com>
2025-08-16 14:10:14 +0200

    Änderungen von Ilyas am 2025-08-16 14:10:14

---
+++ Assets/flying.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spieler : MonoBehaviour
+{
+    
+    public float Geschwindigkeit = 3f;
+    public float Sprungkraft = 3f;
+    public float Sprungkraft0 = 3f;
+    public Rigidbody2D rigidbody;
+
+    Vector2 bewegungsvektor = new Vector2(0,0);
+    Vector2 laufvector = new Vector2(0,0);
+    Vector2 sprungvektor = new Vector2(0,0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bewegungsvektor = new Vector2(0,0);
+        if (Input.GetKey("up"))
+        {
+            bewegungsvektor = bewegungsvektor+Vector2.up*Sprungkraft;
+        }
+
+       
+        if (Input.GetKey("down"))
+        {
+            bewegungsvektor = bewegungsvektor+Vector2.down*Sprungkraft0;
+        }
+
+
+        if (Input.GetKey("right"))
+        {
+            bewegungsvektor = bewegungsvektor+Vector2.right*Geschwindigkeit;
+        }
+
+        if (Input.GetKey("left"))
+        {
+            bewegungsvektor = bewegungsvektor+Vector2.left*Geschwindigkeit;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(bewegungsvektor, ForceMode2D.Impulse);
+    }
+
+
+}
